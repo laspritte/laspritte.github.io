98b221c0f65eec7279b759876c1ee1271c5153f9
Bogeun Jang <a8009608ab@gmail.com>
2023-07-21 16:39:32 +0900

    commit1

--- JS/backgrounds.js
+++ JS/backgrounds.js
@@ -3,6 +3,6 @@ const images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
 const todayImage = images[Math.floor(Math.random()*images.length)]
 
 const image = document.createElement("img");
-image.src = `img/${todayImage}`;
+image.src = `IMG/${todayImage}`;
 
 document.body.appendChild(image)
